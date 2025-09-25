247d26a5e112a289848a62402f63fe2e76f7eee7
nyambushanice07-arch <nyambushanice07@gmail.com>
2025-09-25 07:48:16 +0300

    Add files via upload

---
+++ semesters units.c
@@ -0,0 +1,22 @@
+/*
+Name: Shanice 
+Reg no:CT/G/101/26496/25
+Description: Program to volume 
+*/
+
+
+#include<stdio.h>
+
+int main(){
+// semester's units 
+printf("1.Introduction to programming");
+printf("\n2.ommunication skills ");
+printf("\n3.Computer architecture");
+printf("\n4.Analogue electronics ");
+printf("\n5.Discrete mathematics");
+printf("\n 6.Mathematics for science ");
+printf("\n7.Physics");
+printf ("\n8.Fundamentals of ict");
+
+    return 0;
+}
