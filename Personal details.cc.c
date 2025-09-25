48505868b3b0e7451030c4ed2939d828474d18c5
nyambushanice07-arch <nyambushanice07@gmail.com>
2025-09-25 08:08:32 +0300

    Add files via upload

---
+++ Personal details.cc.c
@@ -0,0 +1,31 @@
+/*
+Name: Shanice 
+Reg no:CT/G/101/26496/25
+Description: program to compute personal details 
+*/
+
+#include<stdio.h>
+int main (){
+//prompt declaration 
+int age;
+float height, tell;
+
+//prompt the user to enter value 
+printf ("enter age value:");
+scanf ("%d",& age);
+
+//prompt the user to enter value 
+printf("enter tell value:");
+scanf("%f",& tell);
+
+//prompt user to enter value 
+printf("\naenter height value:");
+scanf("%f",& height);
+
+
+
+
+
+
+
+}
