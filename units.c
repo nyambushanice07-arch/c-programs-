081b7cbd93329663622e941ed741534909a27607
nyambushanice07-arch <nyambushanice07@gmail.com>
2025-10-02 06:46:54 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,23 @@
+/*  
+    Name: SHANICE 
+    Reg No: CT101/G /26496/25
+    Program: units.c
+    Description: Displays a list of academic units offered this semester.
+ */
+ 
+ #include <stdio.h>
+ 
+int main() {
+    // Display title 
+    printf("Units Offered This Semester:\n"); 
+    printf("-------------------------------\n");
+
+    // Display each unit on a new line
+    printf("1. Introduction to Programming\n");
+    printf("2. Data Structures\n"); 
+    printf("3. Computer Networks\n");
+    printf("4. Database Systems\n"); 
+    printf("5. Software Engineering\n"); 
+
+    return 0;  //execute successful 
+}
